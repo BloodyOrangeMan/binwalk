d217988ab6a37b2389f5303b5b6406bc025861e2
butterfucker3000 <74171212+BloodyOrangeMan@users.noreply.github.com>
2022-12-27 21:14:41 +0800

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -84,7 +84,7 @@ function install_yaffshiv
 function install_sasquatch
 {
     git clone --quiet --depth 1 --branch "master" https://github.com/devttys0/sasquatch
-    (cd sasquatch && $SUDO CFLAGS=-fcommon ./build.sh)
+    (cd sasquatch && CFLAGS=-fcommon ./build.sh)
     $SUDO rm -rf sasquatch
 }
 
